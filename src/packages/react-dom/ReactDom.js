02fa44ab072be20e01d289523182c880a6551510
ynf <1024623671@qq.com>
2025-02-17 15:02:56 +0800

    feat(ReactDom): 实现一个基础版createFiber方法

---
+++ src/packages/react-dom/ReactDom.js
@@ -0,0 +1,42 @@
+import createFiber from "../reconciler/ReactFiber.js";
+
+/**
+ * 更新容器的方法
+ * @param {*} element 要挂载的 vnode 树
+ * @param {*} container 容器的 DOM 节点
+ */
+function updateContainer(element, container) {
+  createFiber(element, {
+    // 该对象就是我的父 fiber 对象，里面会放置一些核心的属性
+    type: container.nodeName.toLowerCase(),
+    stateNode: container,
+  });
+}
+
+class ReactDOMRoot {
+  constructor(container) {
+    // 将拿到的根 DOM 节点在内部保存一份
+    this._internalRoot = container;
+  }
+  /**
+   * 渲染方法
+   * @param {*} children 要挂载到根节点的 vnode 树
+   */
+  render(children) {
+    console.log(children);
+    updateContainer(children, this._internalRoot);
+  }
+}
+
+const ReactDom = {
+  /**
+   * 创建根组件
+   * @param {HTMLElement} container 根 DOM 节点
+   * @returns 返回一个包含 render 方法的对象
+   */
+  createRoot(container) {
+    return new ReactDOMRoot(container);
+  }
+}
+
+export default ReactDom
---
+++ src/packages/reconciler/ReactFiber.js
@@ -0,0 +1,61 @@
+import {isFn, isStr, isUndefined, NoFlags} from "../shared/utils.js";
+import {ClassComponent, Fragment, FunctionComponent, HostComponent, HostText} from "./ReactWorkTags.js";
+
+/**
+ * 创建fiber对象
+ * @param vnode 当前vnode节点
+ * @param returnFiber 父fiber节点
+ */
+function createFiber(vnode, returnFiber) {
+  const fiber = {
+    tag: null,
+    // fiber 的类型
+    type: vnode.type,
+    // key
+    key: vnode.key,
+    // props
+    props: vnode.props,
+    // 存储当前的 fiber 对象所对应的 真实DOM 节点
+    stateNode: null,
+    // 整个 fiber 树是以链表的形式串联起来的，因此需要 child、sibling 保存引用关系
+    // 子 fiber
+    child: null,
+    // 兄弟 fiber
+    sibling: null,
+    // 父 fiber
+    return: returnFiber,
+    // 该 fiber 对象要做的具体操作
+    flags: NoFlags,
+    // 记录当前节点在当前层级下的位置
+    index: null,
+    // 存储旧的 fiber 对象，用于双缓冲
+    alternate: null,
+  };
+
+  const type = vnode.type
+  if (isStr(type)) {
+    fiber.tag = HostComponent;
+  } else if (isFn(type)) {
+    // 函数组件和类组件的 type 都是 function
+    // 通过判断 type 是否有 isReactComponent 属性来判断是否为类组件
+    if (type.prototype.isReactComponent) {
+      // 类组件
+      fiber.tag = ClassComponent;
+    } else {
+      fiber.tag = FunctionComponent;
+    }
+  } else if (isUndefined(type)) {
+    // 没有节点类型，说明是纯文本节点
+    fiber.tag = HostText;
+    // 文本节点是没有 props 属性的，为了兼容其他类型数据结构，增加children字段
+    fiber.props = {
+      children: vnode,
+    };
+  } else {
+    fiber.tag = Fragment;
+  }
+
+  return fiber;
+}
+
+export default createFiber
